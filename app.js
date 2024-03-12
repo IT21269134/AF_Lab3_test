1038ad631234464559f6c42d895741084fe850d7
Chamaku <harinichamathka14@gmail.com>
2024-03-12 09:58:13 +0530

    update

--- app.js
+++ app.js
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 const fs = require("fs");
 fs.readFile("file.txt", "utf8", function (err, data) {
 	if (err) throw err;
 	console.log(data);
 });
-=======
-const fs = require('fs');
-fs.readFile('file.txt', 'utf8', function (err, data) {
- if (err) throw err;
- console.log(data);
-});
->>>>>>> 56f6ed0477e95780a9514f9d328d232dd263fb28
